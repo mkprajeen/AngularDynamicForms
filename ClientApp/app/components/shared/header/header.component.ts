4ae293d63c27891ab2df3022980e2eb352a50ca5
PRAJEEN KUMAR <mkprajeen@hotmail.com>
2017-07-13 17:11:26 +0530

    Added Header component

---
+++ ClientApp/app/components/shared/header/header.component.ts
@@ -0,0 +1,13 @@
+﻿
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'shared-header',
+    templateUrl:'./header.component.html'
+})
+export class HeaderComponent {
+    name: string;
+    constructor() {
+        this.name = 'Sam';
+    }
+}
