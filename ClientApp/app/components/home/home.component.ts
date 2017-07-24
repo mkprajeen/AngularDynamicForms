484d27c53f495dd7bfc4d7d8343845207d02e159
PRAJEEN KUMAR <mkprajeen@hotmail.com>
2017-07-24 13:59:17 +0530

    get data from service

--- ClientApp/app/components/home/home.component.ts
+++ ClientApp/app/components/home/home.component.ts
@@ -12,63 +12,66 @@ import { Global } from '../../shared/global'
 export class HomeComponent {
     msg: string;
     constructor(private _templateBldrSrv: TemplatebuilderService) {
-        this._templateBldrSrv.get(Global.BASE_TEMPLATE_ENDPOINT +'GetSectionsForTemplateGroup?templateGroupId=43')
-            .subscribe(confgs => { this.config = confgs; },
-            error => this.msg = <any>error);
+        //this._templateBldrSrv.get(Global.BASE_TEMPLATE_ENDPOINT +'GetSectionsForTemplateGroup?templateGroupId=43')
+        //    .subscribe(confgs => { this.config = confgs; },
+        //    error => this.msg = <any>error);
+        this.config = this._templateBldrSrv.getfromLocal();
+       
     }
     formSubmitted(value) {
         console.log(value);
     }
-    config: mr_form_field[] =
-    [
-        {
-            mr_form_field_id: 11,
-            data_type: 'string',
-            field_type: 'input',
-            display_label: 'Enter your fisrt name',
-            label: 'Lirst name',
-            required_indicator: true,
-            validationmsg: 'First name is mandatory',
-            minlenth: 6,
-        },
-        {
-            mr_form_field_id: 22,
-            data_type: 'string',
-            field_type: 'input',
-            display_label: 'Enter your last name',
-            label: 'Last name',
-            required_indicator: true,
-            validationmsg: 'Last name is mandatory',
-            minlenth: 6,
-        },
-        {
-            mr_form_field_id: 33,
-            data_type: 'string',
-            field_type: 'select',
-            display_label: 'select your favourite food',
-            label: 'Favourite food',
-            value_options: ['Pizza', 'Hot Dogs', 'Knakworstje', 'Coffee'],
-            validationmsg: 'favourite food is mandatory',
-        },
-        {
-            mr_form_field_id: 44,
-            data_type: 'string',
-            field_type: 'button',
-            label: 'Submit',
-        },
-        {
-            mr_form_field_id: 55,
-            data_type: 'widget',
-            field_type: 'widgettest',
-            widget_type: 'widgettest',
-            mr_form_field_data: {
-                name: 'prajeen',
-                url: 'wwww.test.com',
-                description: 'Desc',
-                sex: ''
-            },
-        }
+    config: mr_form_field[];
+    //config: mr_form_field[] =
+    //[
+    //    {
+    //        mr_form_field_id: 11,
+    //        data_type: 'string',
+    //        field_type: 'input',
+    //        display_label: 'Enter your fisrt name',
+    //        label: 'Lirst name',
+    //        required_indicator: true,
+    //        validationmsg: 'First name is mandatory',
+    //        minlenth: 6,
+    //    },
+    //    {
+    //        mr_form_field_id: 22,
+    //        data_type: 'string',
+    //        field_type: 'input',
+    //        display_label: 'Enter your last name',
+    //        label: 'Last name',
+    //        required_indicator: true,
+    //        validationmsg: 'Last name is mandatory',
+    //        minlenth: 6,
+    //    },
+    //    {
+    //        mr_form_field_id: 33,
+    //        data_type: 'string',
+    //        field_type: 'select',
+    //        display_label: 'select your favourite food',
+    //        label: 'Favourite food',
+    //        value_options: ['Pizza', 'Hot Dogs', 'Knakworstje', 'Coffee'],
+    //        validationmsg: 'favourite food is mandatory',
+    //    },
+    //    {
+    //        mr_form_field_id: 44,
+    //        data_type: 'string',
+    //        field_type: 'button',
+    //        label: 'Submit',
+    //    },
+    //    {
+    //        mr_form_field_id: 55,
+    //        data_type: 'widget',
+    //        field_type: 'widgettest',
+    //        widget_type: 'widgettest',
+    //        mr_form_field_data: {
+    //            name: 'prajeen',
+    //            url: 'wwww.test.com',
+    //            description: 'Desc',
+    //            sex: ''
+    //        },
+    //    }
 
-    ];
+    //];
 
 }
--- ClientApp/app/services/templatebuilder.service.ts
+++ ClientApp/app/services/templatebuilder.service.ts
@@ -7,9 +7,59 @@ import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class TemplatebuilderService {
-    constructor(private _http: Http)
-    { }
+   
+   private data :Array<any> = [
+    {
+        mr_form_field_id: 11,
+        data_type: 'string',
+        field_type: 'input',
+        display_label: 'Enter your fisrt name',
+        label: 'Lirst name',
+        required_indicator: true,
+        validationmsg: 'First name is mandatory',
+        minlenth: 6,
+    },
+    {
+        mr_form_field_id: 22,
+        data_type: 'string',
+        field_type: 'input',
+        display_label: 'Enter your last name',
+        label: 'Last name',
+        required_indicator: true,
+        validationmsg: 'Last name is mandatory',
+        minlenth: 6,
+    },
+    {
+        mr_form_field_id: 33,
+        data_type: 'string',
+        field_type: 'select',
+        display_label: 'select your favourite food',
+        label: 'Favourite food',
+        value_options: ['Pizza', 'Hot Dogs', 'Knakworstje', 'Coffee'],
+        validationmsg: 'favourite food is mandatory',
+    },
+    {
+        mr_form_field_id: 44,
+        data_type: 'string',
+        field_type: 'button',
+        label: 'Submit',
+    },
+    {
+        mr_form_field_id: 55,
+        data_type: 'widget',
+        field_type: 'widgettest',
+        widget_type: 'widgettest',
+        mr_form_field_data: {
+            name: 'prajeen',
+            url: 'wwww.test.com',
+            description: 'Desc',
+            sex: ''
+        },
+    }
 
+    ];
+   constructor(private _http: Http)
+   { }
     get(url: string):Observable<any>
     {
         return this._http.get(url)
@@ -17,6 +67,12 @@ export class TemplatebuilderService {
             // .do(data => console.log("All: " + JSON.stringify(data)))
             .catch(this.handleError);
     }
+    
+    getfromLocal(): Array<any> {
+ 
+      return this.data;
+    }
+
     private handleError(error: Response) {
         console.error(error);
         return Observable.throw(error.json().error || 'Server error');
