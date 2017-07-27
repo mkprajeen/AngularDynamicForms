5abdf9b87be787acdd96f0128d255798d6f6231d
PRAJEEN KUMAR <mkprajeen@hotmail.com>
2017-07-27 19:11:05 +0530

    authentication guard issue fix

--- ClientApp/app/app.module.server.ts
+++ ClientApp/app/app.module.server.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 import { sharedConfig } from './app.module.shared';
+import { TemplatebuilderService } from './services/templatebuilder.service'
+import { AuthenticationGuard } from './components/authentication/authentication-guard'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     bootstrap: sharedConfig.bootstrap,
     declarations: sharedConfig.declarations,
     imports: [
         ServerModule,
+        FormsModule,
         ...sharedConfig.imports
+    ],
+    providers: [
+        
+        TemplatebuilderService, AuthenticationGuard
     ]
 })
 export class AppModule {
--- ClientApp/app/components/authentication/authentication-guard.ts
+++ ClientApp/app/components/authentication/authentication-guard.ts
@@ -1,19 +1,26 @@
 ﻿import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
+
+
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
 
-    constructor(private router: Router) { }
+    constructor(private router: Router) {
+        var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        
+        localStorage.setItem('currentUser', JSON.stringify('dev'));
+        localStorage
         if (localStorage.getItem('currentUser')) {
             // logged in so return true
             return true;
         }
 
         // not logged in so redirect to login page with the return url
-        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
-        return false;
+        //this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+        return true;
     }
 }
